0ea85b0d72061cacff99ba05f9b95e5699aa9965
Charlie Ragone <ragonecharlie@gmail.com>
2024-03-11 23:17:49 -0400

    minor fixes

--- client/src/containers/ContactPage.jsx
+++ client/src/containers/ContactPage.jsx
@@ -8,14 +8,14 @@ const ContactPage = () => {
       <div className="max-w-3xl mx-auto">
         <p className="text-lg mb-4 hover:text-gray-200">For inquiries and further information, please contact us:</p>
         <ul className="list-disc pl-6">
-          <li className="hover:text-gray-200">Email: ragonecharlie@example.com</li>
+          <li className="hover:text-gray-200">Email: ragonecharlie@gmail.com</li>
           <li className="hover:text-gray-200">Phone: (518)878-7754</li>
-          <li className="hover:text-gray-200"><a href="https://www.instagram.com/runclubtc/">Instagram Click Here</a></li>
-          <li className="hover:text-gray-200"><a href="https://www.facebook.com/groups/259027783894516">Facebook Click Here</a></li>
+          <li className="hover:text-gray-200 text-info font-bold"><a href="https://www.instagram.com/runclubtc/">Instagram</a></li>
+          <li className="hover:text-gray-200 text-info font-bold"><a href="https://www.facebook.com/groups/259027783894516">Facebook</a></li>
         </ul>
       </div>
     </div>
   );
 };
 
-export default ContactPage;
+export default ContactPage; 
