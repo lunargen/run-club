bd90ba75dc4de8afb54bd7f44629d70194eb5484
cragone <132158708+cragone@users.noreply.github.com>
2024-03-12 15:07:53 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/DaisyuiNavBar';
 import SponsorPage from './containers/SponsorPage';
 import MissionPage from './containers/MissionPage';
 import ContactPage from './containers/ContactPage';
+/* add favicon by <link rel="icon" href="%PUBLIC_URL%/favicon.ico" /> inbetween head tag */
 
 function App() {
   return (
