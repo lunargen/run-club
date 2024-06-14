d97fcb08c6f34bf77d902055b02f7dfd8b6ab282
cr816145 <ragonecharlie@gmail.com>
2024-06-13 22:59:02 -0400

    lots of changes

--- client/src/containers/HomePage.jsx
+++ client/src/containers/HomePage.jsx
@@ -5,9 +5,9 @@ const HomePage = () => {
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
-      const scrollingText = document.querySelector('.scrolling-text');
-      if (scrollingText) {
-        scrollingText.style.transform = `translateX(-${scrollPosition * 0.5}px)`;
+      const scrollingImage = document.querySelector('.scrolling-image');
+      if (scrollingImage) {
+        scrollingImage.style.transform = `translateX(-${scrollPosition}px)`;
       }
     };
 
@@ -20,7 +20,7 @@ const HomePage = () => {
 
   return (
     <div className="relative w-full">
-      <div className="relative w-full h-screen">
+      <div className="relative w-full h-screen overflow-x-hidden">
         <div className="absolute inset-0 z-0">
           <img
             src="./public/tomua.jpg"
@@ -30,10 +30,13 @@ const HomePage = () => {
         </div>
 
         <div className="absolute bottom-0 right-0 z-10 p-8">
-          <h1 className="text-4xl font-bold text-black scrolling-text text-mono">
-            Run Club
-          </h1>
+          <img
+            src="./public/clubicon.jpg"
+            alt="Scrolling Image"
+            className="scrolling-image w-20 h-20 object-contain"
+          />
         </div>
+
 
       </div>
 
