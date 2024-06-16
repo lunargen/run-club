e3b668ea8d26e9f1084605702dad773318fa4171
cr816145 <ragonecharlie@gmail.com>
2024-06-16 16:13:50 -0400

    ui changes

--- client/src/components/DaisyuiNavBar.jsx
+++ client/src/components/DaisyuiNavBar.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
 
-const Navbar = () => {
-  return (
-    <div className="navbar bg-black text-white z-10 py-2">
-      <div className="flex justify-end w-full px-4">
-        {/* Right Side */}
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <details>
-                <summary className="cursor-pointer font-mono">
-                  About Us
-                </summary>
-                <ul className="p-2 bg-black text-white font-mono rounded-t-none z-10">
-                  <li>
-                    <a href="https://docs.google.com/forms/d/e/1FAIpQLSdGRDxY7h3bLHVBPEFZREcoUdlBMSc2osVkSWXSmi8lAdo3-w/viewform?usp=sf_link">Join Us</a>
-                  </li>
-                  <li><a href="/SponsorPage">Sponsorships</a></li>
-                  <li><a href="/MissionPage">Our Mission</a></li>
-                </ul>
-              </details>
-            </li>
-          </ul>
+
+  const Navbar = () => {
+    return (
+      <div className="navbar bg-black text-white fixed top-0 left-0 w-full py-2 z-50">
+        <div className="flex justify-end w-full px-4">
+          {/* Right Side */}
+          <div className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <details>
+                  <summary className="cursor-pointer font-mono">
+                    About Us
+                  </summary>
+                  <ul className="p-2 bg-black text-white font-mono rounded-t-none z-10">
+                    <li><a href="/">Home</a></li>
+                    <li>
+                      <a href="https://docs.google.com/forms/d/e/1FAIpQLSdGRDxY7h3bLHVBPEFZREcoUdlBMSc2osVkSWXSmi8lAdo3-w/viewform?usp=sf_link">Join Us</a>
+                    </li>
+                    <li><a href="/SponsorPage">Sponsorships</a></li>
+                    <li><a href="/MissionPage">Our Mission</a></li>
+                  </ul>
+                </details>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
   
-    
+  
   
 };
 
--- client/src/containers/SponsorPage.jsx
+++ client/src/containers/SponsorPage.jsx
@@ -2,51 +2,53 @@ import React from "react";
 
 const SponsorPage = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8">Sponsorship Opportunities</h1>
+    <div className="relative min-h-screen">
+      <div className="absolute inset-0 z-0">
+        <img
+          src="./public/tomua.jpg"
+          alt="Banner"
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="relative z-10 container mx-auto px-4 py-8 z-1">
+        <h1 className="text-3xl font-bold mb-8 text-white">Sponsorship Opportunities</h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
-        <div className="border border-gray-200 p-4 rounded-md hover:border-gray-500">
-          <h2 className="text-lg font-semibold mb-4">Base Sponsor</h2>
-          <ol class="ps-5 mt-2 space-y-1 list-decimal list-inside">
-            <li>Pitch your own tent at our event</li>
-            <li>Business name on race shirt</li>
-          </ol>
-        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+          <div className="border border-gray-200 p-4 rounded-md hover:border-gray-500 bg-white bg-opacity-80">
+            <h2 className="text-lg font-semibold mb-4">Base Sponsor</h2>
+            <ol className="ps-5 mt-2 space-y-1 list-decimal list-inside">
+              <li>Get Instagram Shout outs</li>
+            </ol>
+          </div>
 
-        <div className="border border-gray-200 p-4 rounded-md hover:border-gray-500">
-          <h2 className="text-lg font-semibold mb-4">Premier Sponsor</h2>
-          <ol class="ps-5 mt-2 space-y-1 list-decimal list-inside">
-            <li>Pitch your own tent at our event</li>
-            <li>Business name on race shirt</li>
-            <li>Social Media Announcement</li>
-          </ol>
-        </div>
+          <div className="border border-gray-200 p-4 rounded-md hover:border-gray-500 bg-white bg-opacity-80">
+            <h2 className="text-lg font-semibold mb-4">Premier Sponsor</h2>
+            <ol className="ps-5 mt-2 space-y-1 list-decimal list-inside">
+              <li>Get Instagram Shout outs</li>
+              <li>Business name on a released Product</li>
+            </ol>
+          </div>
 
-        <div className="border border-gray-200 p-4 rounded-md hover:border-gray-500">
-          <h2 className="text-lg font-semibold mb-4">Special Sponsor</h2>
-          <ol class="ps-5 mt-2 space-y-1 list-decimal list-inside">
-            <li>Pitch your own tent at our event</li>
-            <li>Business name on race shirt</li>
-            <li>Social Media Announcement</li>
-            <li>10 complimentary event tickets</li>
-            <li>Special Reel made with our athletes for your business</li>
-            <li>One event named after your Business</li>
-          </ol>
-        </div>
+          <div className="border border-gray-200 p-4 rounded-md hover:border-gray-500 bg-white bg-opacity-80">
+            <h2 className="text-lg font-semibold mb-4">Special Sponsor</h2>
+            <ol className="ps-5 mt-2 space-y-1 list-decimal list-inside">
+              <li>Instagram Shout outs</li>
+              <li>Business name on released product</li>
+              <li>Instagram reels made by athletes</li>
+            </ol>
+          </div>
 
-        <div className="border border-gray-200 p-4 rounded-md hover:border-gray-500">
-          <h2 className="text-lg font-semibold mb-4">Title Sponsor</h2>
-          <ol class="ps-5 mt-2 space-y-1 list-decimal list-inside">
-            <li>Title Sponsor</li>
-            <li>Announced as Marquee sponsor of the meet</li>
-            <li>Pitch your own tent at our event</li>
-            <li>Business name big on front of Race shirt</li>
-            <li>Social Media Announcement</li>
-            <li>10 complimentary event tickets</li>
-            <li>Special Reel made with our athletes for your business</li>
-            <li>One race named after your Business</li>
-          </ol>
+          <div className="border border-gray-200 p-4 rounded-md hover:border-gray-500 bg-white bg-opacity-80">
+            <h2 className="text-lg font-semibold mb-4">Title Sponsor</h2>
+            <ol className="ps-5 mt-2 space-y-1 list-decimal list-inside">
+              <li>Title Sponsor</li>
+              <li>Announced as Marquee sponsor of RunClub</li>
+              <li>Instagram shout outs</li>
+              <li>Business products made with your company name</li>
+              <li>Athletes post reels with your branding</li>
+              <li>Brand on the racing jersey</li>
+            </ol>
+          </div>
         </div>
       </div>
     </div>
